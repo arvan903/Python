1e10d52543a0fa03d3f62fa0de29cb23432b1131
arvan903 <arvan903@hotmail.com>
2024-10-23 12:08:00 +0330

    Working

---
+++ Practice Loop List.py
@@ -0,0 +1,9 @@
+programs=["VB","C#","Delphi","Flutter","Dart","SQL"]
+
+for program in programs:
+    print(program)
+
+
+person={"Name": "Rashid", "Age": 30,"City": "Neyshabur"}
+print("Name is: ", person["Name"],"Age is: ",person["Age"],"City is: ",person["City"])
+
